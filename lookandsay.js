f77b74355e68eafaa70473dc3d582dcb611faab6
feliun <felipe.polo@gmail.com>
2019-07-19 17:42:04 +0200

    Cleaner code

--- lookandsay.js
+++ lookandsay.js
@@ -3,27 +3,17 @@ const debug = require('debug')('lookandsay');
 module.exports = (initialValue = 1) => {
   let result = initialValue;
 
-  const toArray = (number) => number.toString().split('');
+  const toMetadata = family => ({ times: family.length, number: family[0] });
+  const toSplit = (sequence) => sequence.split('');
+  const build = ({ times, number }) => `${times}${number}`;
 
   const next = () => {
     debug(`Going to next after current value ${result}...`);
-    const numberSplitted = toArray(result);
-    const families = numberSplitted.reduce((total, digit) => {
-      if (total.length === 0) {
-        total.push([digit])
-      } else {
-        const lastFamily = total[total.length - 1];
-        lastFamily.includes(digit) ? lastFamily.push(digit) : total.push([ digit ]);
-      }
-      return total;
-    }, []);
-    debug(`Families: ${families}`);
-    const familiesInfo = families.map((family) => ({
-      times: family.length,
-      number: family[0],
-    }));
-    debug(`Families info: ${JSON.stringify(familiesInfo)}`);
-    result = parseInt(familiesInfo.map(({ times, number }) => `${times}${number}`).join(''));
+    const families = `${result}`.match(/(.)\1*/g)
+    .map(toSplit)
+    .map(toMetadata)
+    .map(build);
+    result = parseInt(families.join(''));
   };
 
   const getResult = () => result;
